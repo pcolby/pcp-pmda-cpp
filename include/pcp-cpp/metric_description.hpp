c95fa68a286f790e8d36894702b2b0e23b4cb791
Paul Colby <git@colby.id.au>
2013-11-18 15:58:35 +1100

    Switched license from LGPLv3 to Boost 1.0.

--- include/pcp-cpp/metric_description.hpp
+++ include/pcp-cpp/metric_description.hpp
@@ -1,21 +1,7 @@
-/*
-    Copyright 2013 Paul Colby
-
-    This file is part of pmda-cpp.
-
-    Pmda-cpp is free software: you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License as published by the Free
-    Software Foundation, either version 3 of the License, or (at your option)
-    any later version.
-
-    Pmda-cpp is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
-    more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with pmda-cpp.  If not, see <http://www.gnu.org/licenses/>.
-*/
+//               Copyright Paul Colby 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//       (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef __PCP_CPP_METRIC_DESCRIPTION_HPP__
 #define __PCP_CPP_METRIC_DESCRIPTION_HPP__
