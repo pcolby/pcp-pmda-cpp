15278701e2da51af5da6d2f17de05ae593be4a30
Paul Colby <git@colby.id.au>
2013-11-21 17:49:19 +1100

    Cluster 2.

--- example/simple/simple.cpp
+++ example/simple/simple.cpp
@@ -20,6 +20,8 @@
 #include <pcp-cpp/pmda.hpp>
 #include <pcp-cpp/units.hpp>
 
+#include <pcp/impl.h> // For __pmProcessRunTimes.
+
 class simple : public pcp::pmda {
 
 public:
@@ -75,31 +77,51 @@ protected:
 
     virtual pmAtomValue fetch_value(const pcp::pmda::metric_id &metric) const
     {
-        //static int32_t red(0), green(100), blue(200);
-        static int32_t rgb[] = { 0, 100, 200 };
-
         if (metric.cluster == 0) {
             if (metric.item == 0) {
                 return pcp::atom(metric.type, numfetch);
             } else if (metric.item == 1) {
+                static int32_t rgb[] = { 0, 100, 200 };
                 rgb[metric.instance] = (rgb[metric.instance] + 1) % 256;
                 return pcp::atom(metric.type, rgb[metric.instance]);
             }
+        } else if (metric.cluster == 1) {
+            static uint32_t oldfetch(0);
+            static double usr, sys;
+            if (oldfetch < numfetch) {
+                __pmProcessRunTimes(&usr, &sys);
+                oldfetch = numfetch;
+            }
+            return pcp::atom(metric.type, metric.item == 3 ? usr : sys);
         }
 
         switch (metric.cluster) {
+        case 0:
+            switch (metric.item) {
             case 0:
-                switch (metric.item) {
-                    case 0:
-                        return pcp::atom(metric.type, numfetch);
-                    case 1:
-                        rgb[metric.instance] = (rgb[metric.instance] + 1) % 256;
-                        return pcp::atom(metric.type, rgb[metric.instance]);
-                }
-                break;
+                return pcp::atom(metric.type, numfetch);
+            case 1:
+                static int32_t rgb[] = { 0, 100, 200 };
+                rgb[metric.instance] = (rgb[metric.instance] + 1) % 256;
+                return pcp::atom(metric.type, rgb[metric.instance]);
+            }
+            break;
+        case 1: {
+            static uint32_t oldfetch(0);
+            static double usr, sys;
+            if (oldfetch < numfetch) {
+                __pmProcessRunTimes(&usr, &sys);
+                oldfetch = numfetch;
+            }
+            switch (metric.item) {
+            case 2:
+                return pcp::atom(metric.type, usr);
+            case 3:
+                return pcp::atom(metric.type, sys);
+            }
         }
-
         throw pcp::exception(PM_ERR_NYI); // Not Yet Implemented.
+        }
     }
 
 };
