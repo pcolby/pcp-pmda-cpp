48b9da815e1421d999923164cec60011e85dd7a5
Paul Colby <git@colby.id.au>
2013-11-22 18:41:57 +1100

    Moved the callback registration code into the pcp::pmda class - mangling should be safe for callback function pointers (yet to be tested though).

--- include/pcp-cpp/pmda.hpp
+++ include/pcp-cpp/pmda.hpp
@@ -90,7 +90,7 @@ protected:
 
     virtual void init(pmdaInterface &interface)
     {
-        set_pmda_callbacks(interface);
+        register_callbacks(interface);
     }
 
     virtual pcp::metrics_description get_supported_metrics() const = 0;
@@ -99,20 +99,9 @@ protected:
 
     virtual pmAtomValue fetch_value(const metric_id &metric) const = 0;
 
-private:
-    pmda_domain_number_type pmda_domain_number;
-    std::string pmda_name;
+    /* Virtual PMDA callback functions below here. You probably don't
+     * want to override any of these, but you can if you want to. */
 
-    // parseCommandLine
-    // ...?
-
-    bool export_pmns_data(/*dir*/) const
-    {
-        /// @todo
-        return false;
-    }
-
-public:
     virtual int on_pmda_attribute(int ctx, int attr, const char *value,
                                   int length, pmdaExt *pmda)
     {
@@ -168,12 +157,111 @@ public:
         return 0;
     }
 
+    virtual void register_callbacks(pmdaInterface &interface)
+    {
+        interface.version.any.profile = &pmda_profile_callback;
+        interface.version.any.fetch = &pmda_fetch_callback;
+        interface.version.any.desc = &pmda_desc_callback;
+        interface.version.any.instance = &pmda_instance_callback;
+        interface.version.any.text = &pmda_text_callback;
+        interface.version.any.store = &pmda_store_callback;
+        #if PCP_CPP_PMDA_INTERFACE_VERSION >= 5
+        interface.version.five.pmid = &pmda_pmid_callback;
+        interface.version.five.name = &pmda_name_callback;
+        interface.version.five.children = &pmda_children_callback;
+        #endif
+        #if PCP_CPP_PMDA_INTERFACE_VERSION >= 6
+        interface.version.six.attribute = &pmda_attribute_callback;
+        #endif
+
+        interface.version.any.ext->e_resultCallBack; /// @todo
+        interface.version.any.ext->e_fetchCallBack; /// @todo
+        interface.version.any.ext->e_checkCallBack; /// @todo
+        interface.version.any.ext->e_doneCallBack; /// @todo
+        #if PCP_CPP_PMDA_INTERFACE_VERSION >= 5
+        interface.version.any.ext->e_endCallBack; /// @todo
+        #endif
+
+        //pmdaSetFetchCallBack(interface, &pmda_fetch_???_callback);
+    }
+
+private:
+    pmda_domain_number_type pmda_domain_number;
+    std::string pmda_name;
+
+    // parseCommandLine
+    // ...?
+
+    bool export_pmns_data(/*dir*/) const
+    {
+        /// @todo
+        return false;
+    }
+
+    /*
+     * Static callback functions registered by the register_callbacks functions.
+     * These all redirect thier non-static singleton counterparts above.
+     */
+
+    static int pmda_attribute_callback(int ctx, int attr, const char *value,int length, pmdaExt *pmda)
+    {
+        return getInstance()->on_pmda_attribute(ctx, attr, value, length, pmda);
+    }
+
+    static int pmda_children_callback(const char *name, int traverse, char ***kids, int **sts, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_children(name, traverse, kids, sts, pmda);
+    }
+
+    static int pmda_desc_callback(pmID pmid, pmDesc *desc, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_desc(pmid, *desc, *pmda);
+    }
+
+    static int pmda_fetch_callback(int numpmid, pmID *pmidlist, pmResult **resp, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_fetch(numpmid, *pmidlist, *resp, *pmda);
+    }
+
+    static int pmda_fetch_callback_callback(pmdaMetric *mdesc, unsigned int inst, pmAtomValue *avp)
+    {
+        return 0;//pcp::pmda::getInstance()->on_pmda_...;
+    }
+
+    static int pmda_instance_callback(pmInDom indom, int inst, char *name, __pmInResult **result, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_instance(indom, inst, name, *result, *pmda);
+    }
+
+    static int pmda_name_callback(pmID pmid, char ***nameset, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_name(pmid, nameset, pmda);
+    }
+
+    static int pmda_pmid_callback(const char *name, pmID *pmid, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_pmid(name, pmid, pmda);
+    }
+
+    static int pmda_profile_callback(__pmProfile *prof, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_profile(*prof, *pmda);
+    }
+
+    static int pmda_store_callback(pmResult *result, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_store(*result, *pmda);
+    }
+
+    static int pmda_text_callback(int ident, int type, char **buffer, pmdaExt *pmda)
+    {
+        return pcp::pmda::getInstance()->on_pmda_text(ident, type, *buffer, *pmda);
+    }
+
 };
 
 } // pcp namespace.
 
 PCP_CPP_END_NAMESPACE
-
-#include "detail/interface.hpp"
 
 #endif
