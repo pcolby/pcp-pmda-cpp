28aa9fff99700bdb4194c7e6dc2c65c7aa5ee227
Paul Colby <git@colby.id.au>
2013-11-22 18:14:02 +1100

    Some more interface callback work.

--- include/pcp-cpp/detail/interface.hpp
+++ include/pcp-cpp/detail/interface.hpp
@@ -10,6 +10,10 @@
 
 #include <pcp/pmda.h>
 
+namespace pcp {
+
+namespace callback {
+
 extern "C" {
 
 int pmda_attribute_callback(int ctx, int attr, const char *value, int length, pmdaExt *pmda)
@@ -30,6 +34,11 @@ int pmda_desc_callback(pmID pmid, pmDesc *desc, pmdaExt *pmda)
 int pmda_fetch_callback(int numpmid, pmID *pmidlist, pmResult **resp, pmdaExt *pmda)
 {
     return pcp::pmda::getInstance()->on_pmda_fetch(numpmid, *pmidlist, *resp, *pmda);
+}
+
+int pmda_fetch_callback_callback(pmdaMetric *mdesc, unsigned int inst, pmAtomValue *avp)
+{
+    return 0;//pcp::pmda::getInstance()->on_pmda_...;
 }
 
 int pmda_instance_callback(pmInDom indom, int inst, char *name, __pmInResult **result, pmdaExt *pmda)
@@ -64,25 +73,25 @@ int pmda_text_callback(int ident, int type, char **buffer, pmdaExt *pmda)
 
 } // extern "C"
 
-PCP_CPP_BEGIN_NAMESPACE
+} // namespace callback
 
-namespace pcp {
+PCP_CPP_BEGIN_NAMESPACE
 
 void setPmdaCallbacks(pmdaInterface &interface)
 {
-    interface.version.any.profile = &pmda_profile_callback;
-    interface.version.any.fetch = &pmda_fetch_callback;
-    interface.version.any.desc = &pmda_desc_callback;
-    interface.version.any.instance = &pmda_instance_callback;
-    interface.version.any.text = &pmda_text_callback;
-    interface.version.any.store = &pmda_store_callback;
+    interface.version.any.profile = &callback::pmda_profile_callback;
+    interface.version.any.fetch = &callback::pmda_fetch_callback;
+    interface.version.any.desc = &callback::pmda_desc_callback;
+    interface.version.any.instance = &callback::pmda_instance_callback;
+    interface.version.any.text = &callback::pmda_text_callback;
+    interface.version.any.store = &callback::pmda_store_callback;
     #if PCP_CPP_PMDA_INTERFACE_VERSION >= 5
-    interface.version.five.pmid = &pmda_pmid_callback;
-    interface.version.five.name = &pmda_name_callback;
-    interface.version.five.children = &pmda_children_callback;
+    interface.version.five.pmid = &callback::pmda_pmid_callback;
+    interface.version.five.name = &callback::pmda_name_callback;
+    interface.version.five.children = &callback::pmda_children_callback;
     #endif
     #if PCP_CPP_PMDA_INTERFACE_VERSION >= 6
-    interface.version.six.attribute = &pmda_attribute_callback;
+    interface.version.six.attribute = &callback::pmda_attribute_callback;
     #endif
 
     interface.version.any.ext->e_resultCallBack; /// @todo
@@ -92,6 +101,8 @@ void setPmdaCallbacks(pmdaInterface &interface)
     #if PCP_CPP_PMDA_INTERFACE_VERSION >= 5
     interface.version.any.ext->e_endCallBack; /// @todo
     #endif
+
+    //pmdaSetFetchCallBack(interface, &pmda_fetch_???_callback);
 }
 
 } // namespace pcp
