023b132ca6860287661933dc5655b55b91eafa87
Paul Colby <git@colby.id.au>
2013-12-15 18:10:32 +1100

    Correct the semantics of the EXPECT_EQ macros.

--- test/unit/src/main.cpp
+++
@@ -1,3 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(A, B) { SUCCEED(); }
--- test/unit/src/test_units.cpp
+++ test/unit/src/test_units.cpp
@@ -4,50 +4,50 @@
 
 TEST(units, independent_values) {
     const pmUnits new_units = pcp::units(1,2,3, 4,5,6);
-    EXPECT_EQ(new_units.dimSpace,   1);
-    EXPECT_EQ(new_units.dimTime,    2);
-    EXPECT_EQ(new_units.dimCount,   3);
-    EXPECT_EQ(new_units.scaleSpace, 4);
-    EXPECT_EQ(new_units.scaleTime,  5);
-    EXPECT_EQ(new_units.scaleCount, 6);
+    EXPECT_EQ(1, new_units.dimSpace);
+    EXPECT_EQ(2, new_units.dimTime);
+    EXPECT_EQ(3, new_units.dimCount);
+    EXPECT_EQ(4, new_units.scaleSpace);
+    EXPECT_EQ(5, new_units.scaleTime);
+    EXPECT_EQ(6, new_units.scaleCount);
 }
 
 TEST(units, lower_bound) {
     const pmUnits new_units = pcp::units(-8,-8,-8, 0,0,-8);
-    EXPECT_EQ(new_units.dimSpace,   -8);
-    EXPECT_EQ(new_units.dimTime,    -8);
-    EXPECT_EQ(new_units.dimCount,   -8);
-    EXPECT_EQ(new_units.scaleSpace,  0);
-    EXPECT_EQ(new_units.scaleTime,   0);
-    EXPECT_EQ(new_units.scaleCount, -8);
+    EXPECT_EQ(-8, new_units.dimSpace);
+    EXPECT_EQ(-8, new_units.dimTime);
+    EXPECT_EQ(-8, new_units.dimCount);
+    EXPECT_EQ( 0, new_units.scaleSpace);
+    EXPECT_EQ( 0, new_units.scaleTime);
+    EXPECT_EQ(-8, new_units.scaleCount);
 }
 
 TEST(units, upper_bound) {
     const pmUnits new_units = pcp::units(7,7,7, 15,15,7);
-    EXPECT_EQ(new_units.dimSpace,   7);
-    EXPECT_EQ(new_units.dimTime,    7);
-    EXPECT_EQ(new_units.dimCount,   7);
-    EXPECT_EQ(new_units.scaleSpace, 15);
-    EXPECT_EQ(new_units.scaleTime,  15);
-    EXPECT_EQ(new_units.scaleCount, 7);
+    EXPECT_EQ( 7, new_units.dimSpace);
+    EXPECT_EQ( 7, new_units.dimTime);
+    EXPECT_EQ( 7, new_units.dimCount);
+    EXPECT_EQ(15, new_units.scaleSpace);
+    EXPECT_EQ(15, new_units.scaleTime);
+    EXPECT_EQ( 7, new_units.scaleCount);
 }
 
 TEST(units, underflow) {
     const pmUnits new_units = pcp::units(-9,-9,-9, -1,-1,-9);
-    EXPECT_EQ(new_units.dimSpace,    7);
-    EXPECT_EQ(new_units.dimTime,     7);
-    EXPECT_EQ(new_units.dimCount,    7);
-    EXPECT_EQ(new_units.scaleSpace, 15);
-    EXPECT_EQ(new_units.scaleTime,  15);
-    EXPECT_EQ(new_units.scaleCount,  7);
+    EXPECT_EQ( 7, new_units.dimSpace);
+    EXPECT_EQ( 7, new_units.dimTime);
+    EXPECT_EQ( 7, new_units.dimCount);
+    EXPECT_EQ(15, new_units.scaleSpace);
+    EXPECT_EQ(15, new_units.scaleTime);
+    EXPECT_EQ( 7, new_units.scaleCount);
 }
 
 TEST(units, overflow) {
     const pmUnits new_units = pcp::units(8,8,8, 16,16,8);
-    EXPECT_EQ(new_units.dimSpace,   -8);
-    EXPECT_EQ(new_units.dimTime,    -8);
-    EXPECT_EQ(new_units.dimCount,   -8);
-    EXPECT_EQ(new_units.scaleSpace,  0);
-    EXPECT_EQ(new_units.scaleTime,   0);
-    EXPECT_EQ(new_units.scaleCount, -8);
+    EXPECT_EQ(-8, new_units.dimSpace);
+    EXPECT_EQ(-8, new_units.dimTime);
+    EXPECT_EQ(-8, new_units.dimCount);
+    EXPECT_EQ( 0, new_units.scaleSpace);
+    EXPECT_EQ( 0, new_units.scaleTime);
+    EXPECT_EQ(-8, new_units.scaleCount);
 }
