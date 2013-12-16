0654266def8906cf35ff4c5dd51ea8279c5f57d2
Paul Colby <git@colby.id.au>
2013-12-17 08:35:21 +1100

    Added the Boost license info to the unit test sources.

--- test/unit/src/test_exception.cpp
+++ test/unit/src/test_exception.cpp
@@ -1,3 +1,8 @@
+//               Copyright Paul Colby 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//       (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "pcp-cpp/exception.hpp"
 
 #include "gtest/gtest.h"
