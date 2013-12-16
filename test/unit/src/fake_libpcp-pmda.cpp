8ab139fe1d8cd3ed2ffcd346b3023fec8f804015
Paul Colby <git@colby.id.au>
2013-12-16 21:40:10 +1100

    More older PCP versions support.

--- test/unit/src/fake_libpcp-pmda.cpp
+++ test/unit/src/fake_libpcp-pmda.cpp
@@ -1,2 +1,3 @@
-#include <pcp/pmapi.h>
-#include <pcp/pmda.h>
+#include <pcp/pmapi.h> // Note, the order in which these are included matters
+#include <pcp/impl.h>  // more for older versions of PCP, so don't reorder them
+#include <pcp/pmda.h>  // without testing against older versions of PCP.
