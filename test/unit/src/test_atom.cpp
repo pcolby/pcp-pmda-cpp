4ca88503a72cd09260c1597f9280065c95783329
Paul Colby <git@colby.id.au>
2013-12-16 20:11:30 +1100

    More unit tests.

--- test/unit/src/test_atom.cpp
+++ test/unit/src/test_atom.cpp
@@ -47,11 +47,51 @@ TEST(atom, pm_type_u32) {
 }
 
 TEST(atom, pm_type_64) {
-    /// @todo
+    // PM_TYPE_64 should support all signed integer types up to 64 bits.
+    EXPECT_EQ(std::numeric_limits<int8_t>::min(),  pcp::atom(PM_TYPE_64, std::numeric_limits<int8_t>::min()).ll);
+    EXPECT_EQ(std::numeric_limits<int8_t>::max(),  pcp::atom(PM_TYPE_64, std::numeric_limits<int8_t>::max()).ll);
+    EXPECT_EQ(std::numeric_limits<int16_t>::min(), pcp::atom(PM_TYPE_64, std::numeric_limits<int16_t>::min()).ll);
+    EXPECT_EQ(std::numeric_limits<int16_t>::max(), pcp::atom(PM_TYPE_64, std::numeric_limits<int16_t>::max()).ll);
+    EXPECT_EQ(std::numeric_limits<int32_t>::min(), pcp::atom(PM_TYPE_64, std::numeric_limits<int32_t>::min()).ll);
+    EXPECT_EQ(std::numeric_limits<int32_t>::max(), pcp::atom(PM_TYPE_64, std::numeric_limits<int32_t>::max()).ll);
+    EXPECT_EQ(std::numeric_limits<int64_t>::min(), pcp::atom(PM_TYPE_64, std::numeric_limits<int64_t>::min()).ll);
+    EXPECT_EQ(std::numeric_limits<int64_t>::max(), pcp::atom(PM_TYPE_64, std::numeric_limits<int64_t>::max()).ll);
+
+    // PM_TYPE_64 should support floats.
+    EXPECT_FLOAT_EQ(static_cast<float>(-1000.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(-1000.0)).ll);
+    EXPECT_FLOAT_EQ(static_cast<float>(   -1.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(   -1.0)).ll);
+    EXPECT_FLOAT_EQ(static_cast<float>(    0.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(    0.0)).ll);
+    EXPECT_FLOAT_EQ(static_cast<float>(    1.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(    1.0)).ll);
+    EXPECT_FLOAT_EQ(static_cast<float>( 1000.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>( 1000.0)).ll);
+
+    // PM_TYPE_64 should support doubles.
+    EXPECT_DOUBLE_EQ(static_cast<double>(-1000.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(-1000.0)).ll);
+    EXPECT_DOUBLE_EQ(static_cast<double>(   -1.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(   -1.0)).ll);
+    EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    0.0)).ll);
+    EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    1.0)).ll);
+    EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>( 1000.0)).ll);
 }
 
 TEST(atom, pm_type_u64) {
-    /// @todo
+    // PM_TYPE_U64 should support all unsigned integer types up to 64 bits.
+    EXPECT_EQ(std::numeric_limits<uint8_t>::min(),  pcp::atom(PM_TYPE_U64, std::numeric_limits<uint8_t>::min()).ull);
+    EXPECT_EQ(std::numeric_limits<uint8_t>::max(),  pcp::atom(PM_TYPE_U64, std::numeric_limits<uint8_t>::max()).ull);
+    EXPECT_EQ(std::numeric_limits<uint16_t>::min(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint16_t>::min()).ull);
+    EXPECT_EQ(std::numeric_limits<uint16_t>::max(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint16_t>::max()).ull);
+    EXPECT_EQ(std::numeric_limits<uint32_t>::min(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint32_t>::min()).ull);
+    EXPECT_EQ(std::numeric_limits<uint32_t>::max(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint32_t>::max()).ull);
+    EXPECT_EQ(std::numeric_limits<uint64_t>::min(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint64_t>::min()).ull);
+    EXPECT_EQ(std::numeric_limits<uint64_t>::max(), pcp::atom(PM_TYPE_U64, std::numeric_limits<uint64_t>::max()).ull);
+
+    // PM_TYPE_U64 should support positive floats.
+    EXPECT_FLOAT_EQ(static_cast<float>(    0.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(    0.0)).ull);
+    EXPECT_FLOAT_EQ(static_cast<float>(    1.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>(    1.0)).ull);
+    EXPECT_FLOAT_EQ(static_cast<float>( 1000.0), pcp::atom<float>(PM_TYPE_64, static_cast<float>( 1000.0)).ull);
+
+    // PM_TYPE_U64 should support positive doubles.
+    EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    0.0)).ull);
+    EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    1.0)).ull);
+    EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>( 1000.0)).ull);
 }
 
 TEST(atom, pm_type_float) {
