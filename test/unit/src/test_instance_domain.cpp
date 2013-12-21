d30279b96c617cce85d34aad3508f848ba1993c3
Paul Colby <git@colby.id.au>
2013-12-21 13:26:24 +1100

    Fixed some signed/unsigned comparison warnings from older compilers that can't infer the 'best' template instantiation types.

--- test/unit/src/test_instance_domain.cpp
+++ test/unit/src/test_instance_domain.cpp
@@ -44,33 +44,33 @@ TEST(instance_domain, functor_1) {
     indom(std::numeric_limits<pcp::domain_id_type>::min());
     EXPECT_EQ(std::numeric_limits<pcp::domain_id_type>::min(), indom.get_domain_id());
     EXPECT_EQ(PM_INDOM_NULL, indom.get_pm_instance_domain());
-    EXPECT_EQ(0, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(0), indom.size());
 
     indom(std::numeric_limits<pcp::domain_id_type>::max());
     EXPECT_EQ(std::numeric_limits<pcp::domain_id_type>::max(), indom.get_domain_id());
     EXPECT_EQ(PM_INDOM_NULL, indom.get_pm_instance_domain());
-    EXPECT_EQ(0, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(0), indom.size());
 }
 
 TEST(instance_domain, functor_2) {
     pcp::instance_domain indom;
-    EXPECT_EQ(0, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(0), indom.size());
 
     indom(std::numeric_limits<pcp::instance_id_type>::min(), "min");
-    EXPECT_EQ(1, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(1), indom.size());
     EXPECT_EQ("min", indom.at(std::numeric_limits<pcp::instance_id_type>::min()));
 
     indom(std::numeric_limits<pcp::instance_id_type>::max(), "max");
-    EXPECT_EQ(2, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(2), indom.size());
     EXPECT_EQ("max", indom.at(std::numeric_limits<pcp::instance_id_type>::max()));
 
     indom(123, "initial value");
-    EXPECT_EQ(3, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(3), indom.size());
     EXPECT_EQ("initial value", indom.at(123));
 
     // Inserted a new value with an existing key has no effect (std::map behaviour).
     indom(123, "new value");
-    EXPECT_EQ(3, indom.size());
+    EXPECT_EQ(pcp::instance_domain::size_type(3), indom.size());
     EXPECT_EQ("initial value", indom.at(123));
     EXPECT_EQ("min", indom.at(std::numeric_limits<pcp::instance_id_type>::min()));
     EXPECT_EQ("max", indom.at(std::numeric_limits<pcp::instance_id_type>::max()));
