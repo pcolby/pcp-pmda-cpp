91324adffdf791db1f8f6962829f8cb4cea3ec1f
Paul Colby <git@colby.id.au>
2014-02-22 18:16:42 +1100

    Documented the pcp::atom template function.

--- include/pcp-cpp/units.hpp
+++ include/pcp-cpp/units.hpp
@@ -3,6 +3,11 @@
 //       (See accompanying file LICENSE.md or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+/**
+ * @file
+ * @brief Declares and implements the pcp::units template function.
+ */
+
 #ifndef __PCP_CPP_UNITS_HPP__
 #define __PCP_CPP_UNITS_HPP__
 
@@ -14,7 +19,21 @@ namespace pcp {
 
 class instance_domain;
 
-// Wrapper for PMDA_PMUNITS to avoid C++ "extended initializer lists" warnings.
+/**
+ * @brief Wrapper for PMDA_PMUNITS
+ *
+ * This allows C++11-style initialisation lists, while avoiding C++ "extended
+ * initializer lists" warnings.
+ *
+ * @param a The first argument to pass to the PMDA_PMUNITS macro.
+ * @param b The second argument to pass to the PMDA_PMUNITS macro.
+ * @param d The third argument to pass to the PMDA_PMUNITS macro.
+ * @param d The fourth argument to pass to the PMDA_PMUNITS macro.
+ * @param e The fifth argument to pass to the PMDA_PMUNITS macro.
+ * @param f The sixth argument to pass to the PMDA_PMUNITS macro.
+ *
+ * @return A pmUnits value constructed from the supplied arguments.
+ */
 template<typename Type>
 inline pmUnits units(Type a, Type b, Type c, Type d, Type e, Type f)
 {
