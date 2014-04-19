7fed6b528e3f36994596eddf9a83e2df0739068a
Paul Colby <git@colby.id.au>
2014-04-19 16:43:07 +1000

    Some edge case tests for 100% pcp::atom coverage.

--- test/unit/src/test_atom.cpp
+++ test/unit/src/test_atom.cpp
@@ -29,6 +29,10 @@ TEST(atom, pm_type_32) {
     EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>(    0.0)).l);
     EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>(    1.0)).l);
     EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>( 1000.0)).l);
+
+    // PM_TYPE_32 does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_32, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_32, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_u32) {
@@ -49,6 +53,10 @@ TEST(atom, pm_type_u32) {
     EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>(    0.0)).ul);
     EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>(    1.0)).ul);
     EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_32, static_cast<double>( 1000.0)).ul);
+
+    // PM_TYPE_U32 does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_U32, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_U32, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_64) {
@@ -75,6 +83,10 @@ TEST(atom, pm_type_64) {
     EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    0.0)).ll);
     EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    1.0)).ll);
     EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>( 1000.0)).ll);
+
+    // PM_TYPE_64 does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_64, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_64, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_u64) {
@@ -97,12 +109,20 @@ TEST(atom, pm_type_u64) {
     EXPECT_DOUBLE_EQ(static_cast<double>(    0.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    0.0)).ull);
     EXPECT_DOUBLE_EQ(static_cast<double>(    1.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>(    1.0)).ull);
     EXPECT_DOUBLE_EQ(static_cast<double>( 1000.0), pcp::atom<double>(PM_TYPE_64, static_cast<double>( 1000.0)).ull);
+
+    // PM_TYPE_U64 does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_U64, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_U64, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_float) {
     // PM_TYPE_FLOAT should support the full float range.
     EXPECT_EQ(std::numeric_limits<float>::min(), pcp::atom(PM_TYPE_FLOAT, std::numeric_limits<float>::min()).f);
     EXPECT_EQ(std::numeric_limits<float>::max(), pcp::atom(PM_TYPE_FLOAT, std::numeric_limits<float>::max()).f);
+
+    // PM_TYPE_FLOAT does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_FLOAT, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_FLOAT, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_double) {
@@ -111,10 +131,27 @@ TEST(atom, pm_type_double) {
     EXPECT_EQ(std::numeric_limits<float>::max(),  pcp::atom(PM_TYPE_DOUBLE, std::numeric_limits<float>::max()).d);
     EXPECT_EQ(std::numeric_limits<double>::min(), pcp::atom(PM_TYPE_DOUBLE, std::numeric_limits<double>::min()).d);
     EXPECT_EQ(std::numeric_limits<double>::max(), pcp::atom(PM_TYPE_DOUBLE, std::numeric_limits<double>::max()).d);
+
+    // PM_TYPE_DOUBLE does not support string, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_DOUBLE, const_cast<char *>("not a number")), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_DOUBLE, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_string) {
     EXPECT_EQ("test", pcp::atom(PM_TYPE_STRING, const_cast<char *>("test")).cp);
+
+    // PM_TYPE_STRING does not support number, aggregrate, event and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast<  int8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast< int16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast< int32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast< int64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast< uint8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast<uint16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast<uint32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast<uint64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast< float>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, static_cast<double>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_STRING, reinterpret_cast<pmValueBlock *>(NULL)), pcp::exception);
 }
 
 TEST(atom, pm_type_aggregate) {
@@ -122,6 +159,19 @@ TEST(atom, pm_type_aggregate) {
     pmValueBlock stack_value, * const null_pointer = NULL;
     EXPECT_EQ(&stack_value, pcp::atom(PM_TYPE_AGGREGATE, &stack_value).vbp);
     EXPECT_EQ(null_pointer, pcp::atom(PM_TYPE_AGGREGATE, null_pointer).vbp);
+
+    // PM_TYPE_AGGREGATE does not support number, string, and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast<  int8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast< int16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast< int32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast< int64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast< uint8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast<uint16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast<uint32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast<uint64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast< float>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, static_cast<double>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE, const_cast<char *>("not an aggregate")), pcp::exception);
 }
 
 TEST(atom, pm_type_aggregate_static) {
@@ -129,6 +179,19 @@ TEST(atom, pm_type_aggregate_static) {
     pmValueBlock stack_value, * const null_pointer = NULL;
     EXPECT_EQ(&stack_value, pcp::atom(PM_TYPE_AGGREGATE_STATIC, &stack_value).vbp);
     EXPECT_EQ(null_pointer, pcp::atom(PM_TYPE_AGGREGATE_STATIC, null_pointer).vbp);
+
+    // PM_TYPE_AGGREGATE_STATIC does not support number, string, and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast<  int8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast< int16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast< int32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast< int64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast< uint8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast<uint16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast<uint32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast<uint64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast< float>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, static_cast<double>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_AGGREGATE_STATIC, const_cast<char *>("not an aggregate")), pcp::exception);
 }
 
 TEST(atom, pm_type_aggregate_event) {
@@ -136,4 +199,17 @@ TEST(atom, pm_type_aggregate_event) {
     pmValueBlock stack_value, * const null_pointer = NULL;
     EXPECT_EQ(&stack_value, pcp::atom(PM_TYPE_EVENT, &stack_value).vbp);
     EXPECT_EQ(null_pointer, pcp::atom(PM_TYPE_EVENT, null_pointer).vbp);
+
+    // PM_TYPE_EVENT does not support number, string, and unknown types.
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast<  int8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast< int16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast< int32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast< int64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast< uint8_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast<uint16_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast<uint32_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast<uint64_t>(0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast< float>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, static_cast<double>(0.0)), pcp::exception);
+    EXPECT_THROW(pcp::atom(PM_TYPE_EVENT, const_cast<char *>("not an aggregate")), pcp::exception);
 }
