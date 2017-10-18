7fb5fea78d5cb3b39cde0086e8eb89cf0eebfb3b
Paul Colby <git@colby.id.au>
2017-10-18 20:48:53 +1100

    Use --config-exclude for Cppcheck 1.65+ only
    Early Cppcheck versions did not support any such option.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+EXTRA_ARGS=
+
+# Cppcheck 1.65+ supports the --config-exclude option.
+cppcheck --version | grep -iE '^cppcheck ([2-9]|1\.(6[5-9]|[7-9][0-9]|[1-9][0-9]{2}))'
+if [ "$?" -eq 0 ]; then EXTRA_ARGS="$EXTRA_ARGS --config-exclude=/usr/include"; fi
+
 RC=0
 for platform in unix{32,64} win{32{A,W},64}; do
   echo "$platform:"
-  cppcheck --enable=all --error-exitcode=1 \
+  cppcheck $EXTRA_ARGS --enable=all --error-exitcode=1 \
            -I{/usr/,}include --platform=$platform --std={c++03,c++11,posix} \
            --suppress={*:/usr/include/*,missingIncludeSystem} --quiet \
            ./include/pcp-cpp/*.hpp .
