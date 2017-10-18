162196e3897d43af1e9cca506d5381b7b780b9be
Paul Colby <git@colby.id.au>
2017-10-18 21:06:57 +1100

    User concurrency with cppcheck

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -12,7 +12,7 @@ for platform in unix{32,64} win{32{A,W},64}; do
   cppcheck $EXTRA_ARGS --enable=all --error-exitcode=1 \
            -I{/usr/,}include --platform=$platform --std={c++03,c++11,posix} \
            --suppress={*:/usr/include/*,missingIncludeSystem} --quiet \
-           ./include/pcp-cpp/*.hpp .
+           "$@" ./include/pcp-cpp/*.hpp .
   RC=$((RC+$?))
 done
 exit $RC
