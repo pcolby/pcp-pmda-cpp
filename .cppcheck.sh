b375e748c91e98144cd28117aa2013ae9e3415d9
Paul Colby <git@colby.id.au>
2017-10-18 20:25:20 +1100

    Don't use --config-exclude on Travis CI
    Travis' cppcheck version does not support it.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -2,7 +2,7 @@
 RC=0
 for platform in unix{32,64} win{32{A,W},64}; do
   echo "$platform:"
-  cppcheck --config-exclude=/usr/include --enable=all --error-exitcode=1 \
+  cppcheck --enable=all --error-exitcode=1 \
            -I{/usr/,}include --platform=$platform --std={c++03,c++11,posix} \
            --suppress={*:/usr/include/*,missingIncludeSystem} --quiet \
            ./include/pcp-cpp/*.hpp .
