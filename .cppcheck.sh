e50ff1823bdd20a17fb37372ad1f7163528d2c88
Paul Colby <git@colby.id.au>
2017-10-18 21:54:14 +1100

    Conditionally include /usr/include
    And also see if Travis CI can cope with *.hpp files now.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -5,7 +5,10 @@ HPP_FILES=./include/pcp-cpp/*.hpp
 
 # Cppcheck 1.65+ supports the --config-exclude option.
 cppcheck --version | grep -iE '^cppcheck ([2-9]|1\.(6[5-9]|[7-9][0-9]|[1-9][0-9]{2}))'
-if [ "$?" -eq 0 ]; then EXTRA_ARGS="$EXTRA_ARGS --config-exclude=/usr/include"; fi
+if [ "$?" -eq 0 ]; then
+  EXTRA_ARGS="$EXTRA_ARGS --config-exclude=/usr/include"
+  EXTRA_ARGS="$EXTRA_ARGS -I/usr/include"
+fi
 
 # For reasons unknown, Travis CI's current version of Cppcheck (1.61 at the time of
 # writing) is painfully slow when processing *.hpp files. As it is, we don't need to
@@ -14,7 +17,7 @@ if [ "$?" -eq 0 ]; then EXTRA_ARGS="$EXTRA_ARGS --config-exclude=/usr/include"; 
 # @todo Remove this once Travis CI's Cppcheck version no long exhibits this issue.
 if [ -n "$TRAVIS" ]; then
   echo 'Skipping explicit *.hpp args for Travis CI'
-  HPP_FILES=
+  #HPP_FILES=
 fi
 
 RC=0
