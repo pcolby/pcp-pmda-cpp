e839f6ad0963f0c4807e7a554391d87aa5d75ac2
Paul Colby <git@colby.id.au>
2017-10-18 20:18:34 +1100

    Try a beskpoke .cppcheck script

---
+++ .cppcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+RC=0
+for platform in unix{32,64} win{32{A,W},64}; do
+  echo "$platform:"
+  cppcheck --config-exclude=/usr/include --enable=all --error-exitcode=1 \
+           -I{/usr/,}include --platform=$platform --std={c++03,c++11,posix} \
+           --suppress={*:/usr/include/*,missingIncludeSystem} --quiet \
+           ./include/pcp-cpp/*.hpp .
+  RC=$((RC+$?))
+done
+exit $RC
